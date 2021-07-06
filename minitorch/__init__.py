2b54f774a264da16f0dc10b730905dff20b78523
Rosa Limachi <rlimachip@uni.pe>
2021-07-06 02:43:57 -0500

    Modulo-0

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -1,13 +1,2 @@
 from .module import *  # noqa: F401,F403
 from .operators import *  # noqa: F401,F403
-
-import visdom
-import matplotlib.pyplot as plt
-vis = visdom.Visdom()
-
-# Loss goes down!
-plt.plot([2.0, 1.0, 0.0], c="blue")
-plt.title("Model Loss")
-
-# Send to visdom
-vis.matplot(plt, win="loss")
